84ae3ff9aa1e750c8f662a3b08e9128e318f9870
Chazz Chandler <chazzchandler@u.boisestate.edu>
2020-09-27 22:04:06 -0600

    Finished Accept, addTransition, getToState

--- fa/dfa/DFA.java
+++ fa/dfa/DFA.java
@@ -1,23 +1,49 @@
 package fa.dfa;
+import java.util.HashMap;
 import java.util.HashSet;
 import fa.State;
-
+import java.util.Map;
 import java.util.Set;
 
 public class DFA implements DFAInterface {
     Set<State> qNotArray = new HashSet<State>();
     Set<State> qArray = new HashSet<State>();
     Set<State> fArray = new HashSet<State>();
-    Set<Character> sigma = new HashSet<Character>();
+    Set<Character> sigma = new HashSet<Character>();//Alphabet Array
+    Map<Pair<Character, String>, String> tMap = new HashMap<Pair<Character,String>, String>();
+    private Object Pair;
 
     @Override
     public boolean accepts(String s) {
-        return false;
+
+        Boolean acceptor;
+
+        if(qArray.contains(s)){
+            acceptor = true;
+        } else {
+            acceptor = false;
+        }
+
+        return acceptor;
     }
 
     @Override
     public State getToState(DFAState from, char onSymb) {
-        return null;
+
+        DFAState newState = new DFAState();
+
+
+
+        Pair pair = new Pair(from,onSymb);
+        String s = from.getName();
+
+        Pair p = new Pair(tMap.)
+
+        if(tMap.getOrDefault(from, "nope") == s){
+
+        }
+
+        return newState;
     }
 
     @Override
@@ -34,6 +60,13 @@ public class DFA implements DFAInterface {
         DFAState qState = new DFAState();
         qState.setName(name);
         qArray.add(qState);
+
+        char c = name.charAt(0);
+
+        if(!sigma.contains(name)){//Alphabet Check/adder
+            sigma.add(c);
+        }
+
     }
 
     @Override
@@ -47,6 +80,10 @@ public class DFA implements DFAInterface {
 
     @Override
     public void addTransition(String fromState, char onSymb, String toState) {
+
+        Pair p = new Pair(onSymb, fromState);
+
+        tMap.putIfAbsent(p, toState);
 
     }
 
@@ -67,11 +104,32 @@ public class DFA implements DFAInterface {
 
     @Override
     public Set<Character> getABC() {
-        return null;
+        return sigma;
     }
 
     @Override
     public DFA complement() {
-        return null;
+        DFA compDFA = new DFA();
+
+        while (this.qArray.iterator().hasNext()){
+            compDFA.addState(qArray.iterator().next().getName());
+        }
+
+        compDFA.addStartState(qNotArray.iterator().next().getName());
+
+        while (compDFA.getStates().iterator().hasNext()){
+            String compName = compDFA.getStates().iterator().next().getName();
+
+            while (this.fArray.iterator().hasNext()){
+                State finalState = this.fArray.iterator().next();
+
+                if (compName != finalState.getName()){
+                    compDFA.addFinalState(compName);
+                }
+            }
+        }
+
+        return compDFA;
+    }
     }
 }
